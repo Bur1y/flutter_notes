92271a3b189b9929718c2b4de753ae6489db506a
Sejoja <kotomkin52@mail.ru>
2023-07-26 17:16:31 +0300

    Write crud with sqlflite

---
+++ lib/Database/Database_helper.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/foundation.dart';
+import 'package:sqflite/sqflite.dart' as sql;
+
+class DatabaseHelper {
+
+
+  static Future<sql.Database> db() async {
+    return sql.openDatabase(
+      'Notes.db',
+      version: 1,
+      onCreate: (sql.Database database, int version) async {
+        await createTables(database);
+      },
+    );
+  }
+
+  static Future<void> createTables(sql.Database database) async {
+    await database.execute("""CREATE TABLE notes(
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        title TEXT,
+        description TEXT,
+        createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+      )
+      """);
+  }
+
+  static Future<int> createNote(String? title, String? descrption) async {
+    final db = await DatabaseHelper.db();
+
+    final data = {'title': title, 'description': descrption};
+    final id = await db.insert('notes', data,
+        conflictAlgorithm: sql.ConflictAlgorithm.replace);
+    return id;
+  }
+
+  // Read all items
+  static Future<List<Map<String, dynamic>>> getNotes() async {
+    final db = await DatabaseHelper.db();
+    return db.query('notes', orderBy: "id");
+  }
+
+  static Future<List<Map<String, dynamic>>> getNote(int id) async {
+    final db = await DatabaseHelper.db();
+    return db.query('notes', where: "id = ?", whereArgs: [id], limit: 1);
+  }
+
+  // Update an item by id
+  static Future<int> updateNote(
+      int id, String title, String? descrption) async {
+    final db = await DatabaseHelper.db();
+
+    final data = {
+      'title': title,
+      'description': descrption,
+      'createdAt': DateTime.now().toString()
+    };
+
+    final result =
+    await db.update('notes', data, where: "id = ?", whereArgs: [id]);
+    return result;
+  }
+
+  // Delete
+  static Future<void> deleteNote(int id) async {
+    final db = await DatabaseHelper.db();
+    try {
+      await db.delete("notes", where: "id = ?", whereArgs: [id]);
+    } catch (err) {
+      debugPrint("Всё пиздец - удаление отъебнуло, пиздуй смотреть трассировку: $err");
+    }
+  }
+}
