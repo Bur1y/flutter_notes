bea6d6d78ba936eef4696217441c0f9cc8a8c9c9
Sejoja <kotomkin52@mail.ru>
2023-07-27 16:12:27 +0300

    Update widgets UI and connect sql

--- lib/Widgets/main_menu.dart
+++ lib/Widgets/main_menu.dart
@@ -1,14 +1,56 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter_notes/Database/Database_helper.dart';
 
-/** 25.07.2023 Bur1y
- * Main menu screen
- *
- *
- */
+class main_menu extends StatefulWidget {
+  const main_menu({Key? key}) : super(key: key);
 
-class main_menu extends StatelessWidget {
-  const main_menu({super.key});
+  @override
+  _MainMenuState createState() => _MainMenuState();
+}
+
+class _MainMenuState extends State<main_menu> {
+  List<Map<String, dynamic>> myData = [];
+  final formKey = GlobalKey<FormState>();
+
+  bool _isLoading = true;
+
+  void _refreshData() async {
+    final data = await DatabaseHelper.getNotes();
+    setState(() {
+      myData = data;
+      _isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _refreshData();
+  }
+
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+
+  Future<void> addNote() async {
+    await DatabaseHelper.createNote(
+        _titleController.text, _descriptionController.text);
+    _refreshData();
+  }
+
+  // Update an existing data
+  Future<void> updateItem(int id) async {
+    await DatabaseHelper.updateNote(
+        id, _titleController.text, _descriptionController.text);
+    _refreshData();
+  }
+
+  // Delete an item
+  void deleteItem(int id) async {
+    await DatabaseHelper.deleteNote(id);
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('Successfully deleted!', style: TextStyle(color: Colors.white),), backgroundColor: Colors.black54));
+    _refreshData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,51 +75,72 @@ class main_menu extends StatelessWidget {
             ),
           ];
         },
-        body: SafeArea(
-          top: false,
-          bottom: false,
-          child: Builder(
-            builder: (BuildContext context) {
-
-              return CustomScrollView(
-                slivers: <Widget>[
-                  SliverOverlapInjector(
-                    handle: NestedScrollView.sliverOverlapAbsorberHandleFor(
-                        context),
-                  ),
-                  SliverPadding(
-                    padding: const EdgeInsets.all(8.0),
-                    sliver: SliverFixedExtentList(
-                      itemExtent: 80.0,
-                      delegate: SliverChildBuilderDelegate(
-                            (BuildContext context, int index) {
-                          return _noteCard();
-                        },
-                        childCount: 10,
+        body: _isLoading
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : myData.isEmpty
+                ? const Center(child: Text("No Data Available"))
+                : Container( padding: const EdgeInsets.all(8.0),
+                  margin: EdgeInsets.only(top: 50),
+                  child: Container(
+                    child: ListView.builder(
+                        itemCount: myData.length,
+                        itemBuilder: (context, index) => Card(
+                          color: index % 2 == 0 ? Colors.black54: Colors.black87,
+                          child: Dismissible(
+                            key: UniqueKey(),
+                            background: Container(color: Colors.redAccent),
+                            onDismissed: (direction) {
+                              deleteItem(myData[index]['id']);
+                            },
+                            child: ListTile(
+                              leading: FlutterLogo(size: 56.0),
+                              title: Text(myData[index]['title']),
+                              subtitle: Text(myData[index]['createdAt']),
+                              onTap: () {
+                                // Navigator.push(context,
+                                //     MaterialPageRoute<Widget>(builder: (BuildContext context) {
+                                //
+                                //       return edit_note();
+                                //     }
+                                //     )
+                                // );
+                              },
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
                   ),
-                ],
-              );
-            },
-          ),
+                ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Navigator.push(context,
+          //     MaterialPageRoute<Widget>(builder: (BuildContext context) {
+          //       return edit_note();
+          //       }));
+          print("КНОПКА НАЖАЛА");
+          addNote();
+        },
+        backgroundColor: Colors.redAccent,
+        child: const Icon(
+          Icons.note_add_outlined,
+          size: 28,
         ),
       ),
-      floatingActionButton: _floatingButton(),
     );
   }
 }
 
 // Card view
 class _noteCard extends StatelessWidget {
-
-  const _noteCard( {
+  const _noteCard({
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     return Card(
       child: ListTile(
         leading: FlutterLogo(size: 56.0),
@@ -124,30 +187,6 @@ class _safeAreaTitle extends StatelessWidget {
   }
 }
 
-// Float button
-class _floatingButton extends StatelessWidget {
-  const _floatingButton({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return FloatingActionButton(
-      onPressed: () async {
-        // Navigator.push(context,
-        //     MaterialPageRoute<Widget>(builder: (BuildContext context) {
-        //       return edit_note();
-        //       }));
-      },
-      backgroundColor: Colors.redAccent,
-      child: const Icon(
-        Icons.note_add_outlined,
-        size: 28,
-      ),
-    );
-  }
-}
-
 // Area AppBar buttons
 class _safeAreaBarButtons extends StatelessWidget {
   const _safeAreaBarButtons({
@@ -158,24 +197,24 @@ class _safeAreaBarButtons extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            IconButton(
-              iconSize: 24,
-              icon: const Icon(Icons.picture_as_pdf_outlined),
-              onPressed: () {},
-            ),
-            IconButton(
-              iconSize: 24,
-              icon: const Icon(Icons.find_in_page_outlined),
-              onPressed: () {},
-            ),
-            IconButton(
-              iconSize: 24,
-              icon: const Icon(Icons.more_vert_outlined),
-              onPressed: () {},
-            ),
-          ],
-        ));
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        IconButton(
+          iconSize: 24,
+          icon: const Icon(Icons.picture_as_pdf_outlined),
+          onPressed: () {},
+        ),
+        IconButton(
+          iconSize: 24,
+          icon: const Icon(Icons.find_in_page_outlined),
+          onPressed: () {},
+        ),
+        IconButton(
+          iconSize: 24,
+          icon: const Icon(Icons.more_vert_outlined),
+          onPressed: () {},
+        ),
+      ],
+    ));
   }
 }
